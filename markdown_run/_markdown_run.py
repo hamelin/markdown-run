382f3dd732d2f158e69294a08acd4c5d66253719
Benoit Hamelin <benoit@benoithamelin.com>
2024-08-09 20:36:54 -0400

    Rfct: handle a single read of the file
    Also regularize contextualizing the instantiation of the Mistletoe
    Document under context of the HTMLRenderer.

--- markdown_run/_markdown_run.py
+++ markdown_run/_markdown_run.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import itertools as it
-from mistletoe import Document  # type: ignore
+from mistletoe import Document, HTMLRenderer  # type: ignore
 from mistletoe.block_token import BlockCode, CodeFence  # type: ignore
 import os
 from pathlib import Path
@@ -16,16 +16,17 @@ def extract_code_and_output(
 ) -> Tuple[str, int]:
     assert line >= 0
     path = Path(path_)
-    with path.open(mode="r", encoding="utf-8") as file:
-        doc = Document(file)
+    lines_note = path.read_text().split("\n")
+    with HTMLRenderer():
+        doc = Document(lines_note)
     if not doc.children:
         raise NoCodeThere(path, line)
-    last_line = len(path.read_text().split("\n"))
-    if line > last_line:
+    line_last = len(lines_note)
+    if line > line_last:
         raise NoCodeThere(path, line)
 
     for i, (line_start, line_end) in enumerate(
-        it.pairwise([ch.line_number for ch in doc.children] + [last_line + 1])
+        it.pairwise([ch.line_number for ch in doc.children] + [line_last + 1])
     ):
         if line >= line_start and line < line_end:
             snippet = doc.children[i]
